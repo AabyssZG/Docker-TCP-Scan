a780fa5e3d49c0e41e1b90dcaccb24e54d1d3cf1
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2024-08-19 16:00:48 +0800

    Update Docker-TCP-Scan.py

--- Docker-TCP-Scan.py
+++ Docker-TCP-Scan.py
@@ -9,7 +9,7 @@ import argparse, sys, time
 
 def get_parser():
     parser = argparse.ArgumentParser(usage='python3 Docker-TCP-Scan.py',description='Docker-TCP-Scan: 针对Docker的TCP Socket开源渗透框架',)
-    p = parser.add_argument_group('SpringBoot-Scan 的参数')
+    p = parser.add_argument_group('Docker-TCP-Scan 的参数')
     p.add_argument("-u", "--url", type=str, help="对单一URL进行Docker端点探测")
     p.add_argument("-uf", "--urlfile", type=str, help="读取目标TXT进行Docker端点探测")
     p.add_argument("-d", "--dump", type=str, help="下载指定Docker端点的容器日志（可提取敏感信息）")
